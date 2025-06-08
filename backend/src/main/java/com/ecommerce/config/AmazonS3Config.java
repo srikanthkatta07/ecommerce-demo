0f03c73a1f0b9fde67a6b954026d773eacfc6484
Srikanth Katta <srikanth@everest.engineering>
2025-06-08 12:24:17 +0530

    Add values

--- backend/src/main/java/com/ecommerce/config/AmazonS3Config.java
+++ backend/src/main/java/com/ecommerce/config/AmazonS3Config.java
@@ -4,18 +4,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonS3Config {
 
+  @Value("${aws.s3.access-key}")
+  private String accessKey;
+
+  @Value("${aws.s3.secret-key}")
+  private String secretKey;
+
+  @Value("${aws.s3.region}")
+  private String region;
+
   @Bean
   public AmazonS3 amazonS3() {
-    String accessKey = System.getenv("AWS_ACCESS_KEY"); // Fetch from environment variables
-    String secretKey = System.getenv("AWS_SECRET_KEY"); // Fetch from environment variables
-    String region = System.getenv("AWS_REGION"); // Fetch from environment variables
-
     BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
     return AmazonS3ClientBuilder.standard()
         .withRegion(region)
